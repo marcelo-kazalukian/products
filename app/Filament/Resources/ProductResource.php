9d3dbdd66d57cdad1b0276db13a74e4da7d1274d
Marcelo Kazalukian <marcelok1985@gmail.com>
2025-01-08 22:00:02 +1100

    lesson 09

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -19,6 +19,12 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static array $statuses = [
+        'in stock' => 'in stock',
+        'sold out' => 'sold out',
+        'coming soon' => 'coming soon',
+    ];
+
     public static function form(Form $form): Form
     {
         return $form
@@ -27,6 +33,11 @@ class ProductResource extends Resource
                     ->required()
                     ->unique(ignoreRecord: true),
                 Forms\Components\TextInput::make('price')->required(),
+                Forms\Components\Radio::make('status')
+                    ->options(self::$statuses),
+                Forms\Components\Select::make('category_id') 
+                    ->relationship('category', 'name'),
+                
             ]);
     }
 
@@ -36,16 +47,52 @@ class ProductResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->sortable()
-                    ->searchable(isIndividual: true, isGlobal: false),
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('price')
                     ->money('aud') 
                     ->getStateUsing(function (Product $record): float { 
                         return $record->price / 100; 
+                    })
+                    ->alignEnd(),
+                Tables\Columns\TextColumn::make('category.name')->label('Category name'),
+                Tables\Columns\TextColumn::make('tags.name')->badge(),
+                Tables\Columns\TextColumn::make('status')->badge()->color(fn (string $state): string => match ($state) {
+                        'in stock' => 'primary',
+                        'sold out' => 'danger',
+                        'coming soon' => 'info',
                     }),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->date(),
             ])
             ->filters([
-                //
-            ])
+                Tables\Filters\SelectFilter::make('status')
+                    ->options(self::$statuses),
+                Tables\Filters\SelectFilter::make('category')
+                    ->relationship('category', 'name'),
+                Tables\Filters\Filter::make('created_from')
+                    ->form([
+                        Forms\Components\DatePicker::make('created_from'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            );
+                    }),
+                Tables\Filters\Filter::make('created_until')
+                    ->form([
+                        Forms\Components\DatePicker::make('created_until'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_until'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
+            ], layout: Tables\Enums\FiltersLayout::AboveContent)
+            ->filtersFormColumns(4)
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
@@ -60,7 +107,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TagsRelationManager::class,
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,17 +20,67 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'marcelok1985@gmail.com',
+            'email' => 'm@m.com',
+        ]);
+
+        Category::create([
+            'name' => 'Computers',
+        ]);
+
+        Category::create([
+            'name' => 'Books',
+        ]);
+
+        Category::create([
+            'name' => 'Food',
         ]);
 
         Product::create([
-            'name' => 'Product 1',
+            'name' => 'IBM PS/1',
             'price' => 1000,
+            'category_id' => 1,
         ]);
 
         Product::create([
-            'name' => 'Product 2',
+            'name' => 'Atomic Habits',
             'price' => 2000,
+            'category_id' => 2,
+        ]);
+
+        Product::create([
+            'name' => 'Pizza',
+            'price' => 3000,
+            'category_id' => 3,
+        ]);
+
+        Product::create([
+            'name' => 'Macbook Pro',
+            'price' => 4000,
+            'category_id' => 1,
+        ]);
+
+        Product::create([
+            'name' => 'The Lean Startup',
+            'price' => 5000,
+            'category_id' => 2,
+        ]);
+
+        Product::create([
+            'name' => 'Hamburger',
+            'price' => 6000,
+            'category_id' => 3,
+        ]);
+
+        Tag::create([
+            'name' => 'On Sale',
+        ]);
+
+        Tag::create([
+            'name' => 'New Edition',
+        ]);
+
+        Tag::create([
+            'name' => 'trending',
         ]);
     }
 }
